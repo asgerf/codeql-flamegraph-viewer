ba956908ce698593d21308e082f569b40f4e3cb0
Asger Feldthaus <asgerf@github.com>
2020-08-08 11:15:50 +0100

    Print a nice table

--- tools/stats.sh
+++ tools/stats.sh
@@ -7,13 +7,28 @@ BaseDir="$MyDir/.."
 cd $BaseDir
 
 LogFile='example-data/example.logfile.log'
-LogFileGz='scrap/example.logfile.log.gz'
 TraceFile='scrap/example.trace.json'
+LogFileGz='scrap/example.logfile.log.gz'
 TraceFileGz="$TraceFile.gz"
+LogFileXz='scrap/example.logfile.log.xz'
+TraceFileXz="$TraceFile.xz"
 
 node dist/qlprof $LogFile -o $TraceFile
 
 gzip -c $LogFile > $LogFileGz
 gzip -c $TraceFile > $TraceFileGz
 
-du -sh $LogFile $TraceFile $LogFileGz $TraceFileGz
+xz -c $LogFile > $LogFileXz
+xz -c $TraceFile > $TraceFileXz
+
+function getsize {
+    du -sh $1 | awk '{print $1}'
+}
+
+function makeTable {
+    echo '.' 'Plain' '.gz' '.xz'
+    echo 'Logfile' $(getsize $LogFile) $(getsize $LogFileGz) $(getsize $LogFileXz)
+    echo 'Trace' $(getsize $TraceFile) $(getsize $TraceFileGz) $(getsize $TraceFileXz)
+}
+
+makeTable | column -t
